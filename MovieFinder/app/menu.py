83edfe93bc029c9ee97c9bb0c7817ab050f55925
Luan Souza <luanetec@yahoo.com.br>
2026-08-08 12:20:51 -0300

    Adiciona validação de seleção de filmes

--- MovieFinder/app/menu.py
+++ MovieFinder/app/menu.py
@@ -1,4 +1,12 @@
 def selecionar_filme(lista_de_filmes):
-    numero_filme = int(input('Digite o numero do filme: '))
-    numero_filme -= 1
-    return lista_de_filmes[numero_filme]
+    while True:
+        try:
+            numero_filme = int(input('Digite o numero do filme: '))
+            if numero_filme > 0:
+                numero_filme -= 1
+                return lista_de_filmes[numero_filme]
+            else:
+                print('Opção invalida! Digite novamente')
+        except (ValueError, IndexError):
+            print('Opção invalida! Digite novamente')
+
