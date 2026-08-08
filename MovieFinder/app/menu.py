a4a362a41f429df46cf31533af359e10218dee3a
Luan Souza <luanetec@yahoo.com.br>
2026-08-08 12:45:54 -0300

    Implementa busca interativa de filmes

--- MovieFinder/app/menu.py
+++ MovieFinder/app/menu.py
@@ -1,3 +1,13 @@
+def mostrar_menu():
+    print('=' * 25)
+    print('MOVIEFINDER'.center(25))
+    print('=' * 25)
+    print('1 - Buscar filme')
+    print('2 - Sair')
+    print('=' * 25)
+    opcao = int(input('Escolha uma opção: '))
+    return opcao
+
 def selecionar_filme(lista_de_filmes):
     while True:
         try:
@@ -9,4 +19,5 @@ def selecionar_filme(lista_de_filmes):
                 print('Opção invalida! Digite novamente')
         except (ValueError, IndexError):
             print('Opção invalida! Digite novamente')
+
 
--- MovieFinder/main.py
+++ MovieFinder/main.py
@@ -1,11 +1,20 @@
 from app.api import buscar_filme,buscar_detalhes
 from app.formatador import mostrar_resultado
-from app.menu import selecionar_filme
+from app.menu import selecionar_filme, mostrar_menu
 
-lista_de_filmes = buscar_filme('batman')
+while True:
+    opcao = mostrar_menu()
 
-mostrar_resultado(lista_de_filmes)
-filme = selecionar_filme(lista_de_filmes)
-buscar_detalhes(filme)
-print(filme)
+    if opcao == 1:
+        nome_do_filme = str(input('Digite o nome do filme: '))
+        lista_de_filmes = buscar_filme(nome_do_filme)
+
+        mostrar_resultado(lista_de_filmes)
+        filme = selecionar_filme(lista_de_filmes)
+        buscar_detalhes(filme)
+        print(filme)
+    if opcao == 2:
+        break
+
+
 
